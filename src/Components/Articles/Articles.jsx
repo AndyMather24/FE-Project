831d07f12e80f8494a8c92fc027dc74ef1ef054f
Andrew Mather <Andy-mather@live.co.uk>
2018-11-08 17:15:15 +0000

    article component with styling added

---
+++ src/Components/Articles/Articles.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import * as api from '../../api.js';
+import './Articles.css';
+class Articles extends Component {
+  state = {
+    articles: []
+  };
+  render() {
+    return (
+      <div className="articles-container">
+        {this.state.articles.length &&
+          this.state.articles.map(article => {
+            return (
+              <div key={article._id} className="article-info-container fade">
+                {console.log(article.title)}
+                {article.belongs_to === 'football' && <img src="https://source.unsplash.com/collection/2540303/125x125" alt="placeholder img" />}
+                {article.belongs_to === 'coding' && <img src="https://source.unsplash.com/collection/1129594/125x125" alt="placeholder img" />}
+                {article.belongs_to === 'cooking' && <img src="https://source.unsplash.com/collection/630995/125x125" alt="placeholder img" />}
+                <h6 className="title">{article.title}</h6>
+                <p className="votes">Likes:{article.votes}</p>
+                <p className="comments">Comments: {article.comment_count}</p>
+              </div>
+            );
+          })}
+      </div>
+    );
+  }
+  componentDidMount = () => {
+    console.log('Mounting articles');
+    api.fetchArticles().then(articles => {
+      this.setState({
+        articles: articles
+      });
+    });
+  };
+}
+
+export default Articles;
