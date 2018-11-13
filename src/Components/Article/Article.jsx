9252166f8894aa5bb028ee5b7aeeb1389031f7c0
Andrew Mather <Andy-mather@live.co.uk>
2018-11-13 14:21:42 +0000

    voting added to condition render when article.vote is defined

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import * as api from '../../api';
 import './Article.css';
 import Comments from '../Comments/Comments';
+import Voting from '../Voting/Voting';
 class Article extends Component {
   state = {
     article: {},
@@ -13,12 +14,14 @@ class Article extends Component {
     return (
       <article>
         <figure>
-          <img className="article-image" src="https://source.unsplash.com/collection/1129594/1000x400" alt="article image" />
+          <img className="article-image" src="https://source.unsplash.com/collection/1129594/1000x400" alt="article" />
         </figure>
         <h1>{title}</h1>
 
         <p>{body}</p>
+
         <div className="user-info">
+          <div className="votes-section">{this.state.article.votes && <Voting article={this.state.article} />}</div>
           <img className="user-profile-picture" src="https://source.unsplash.com/collection/895539/70x70" alt="user profile" />
           <p className="username"> Published by: {username}</p>
         </div>
