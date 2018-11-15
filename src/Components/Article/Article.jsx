f2f17aae1fab151c89d7c93c685f50608bad3a09
Andrew Mather <Andy-mather@live.co.uk>
2018-11-15 10:50:50 +0000

    grid added to article  with class name

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -12,21 +12,22 @@ class Article extends Component {
     const { title, body } = this.state.article;
     const { username } = this.state.author;
     return (
-      <article>
-        <figure>
-          <img className="article-image" src="https://source.unsplash.com/collection/1129594/1000x400" alt="article" />
-        </figure>
-        <h1>{title}</h1>
+      <article className="article-container">
+        <div className="article-img">
+          <img src="https://source.unsplash.com/collection/1129594/1000x800" />
+        </div>
+        <h1 className="article-title">{title}</h1>
 
-        <p>{body}</p>
+        <p className="article-body">{body}</p>
 
         <div className="user-info">
-          <div className="votes-section">{this.state.article.votes && <Voting article={this.state.article} />}</div>
           <img className="user-profile-picture" src="https://source.unsplash.com/collection/895539/70x70" alt="user profile" />
           <p className="username"> Published by: {username}</p>
         </div>
 
-        <div>
+        <div className="votes-section">{this.state.article.votes && <Voting article={this.state.article} />}</div>
+
+        <div className="comments">
           <Comments user={this.props.user} article_id={this.props.article_id} />
         </div>
       </article>
