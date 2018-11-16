cbe9839cc047533c2fb3196a5ef121266c953491
Andrew Mather <Andy-mather@live.co.uk>
2018-11-16 16:50:18 +0000

    styling and bug fixes

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -9,7 +9,7 @@ const Dashboard = props => {
         return (
           <div className={topic.title.concat(' fade')} key={topic._id}>
             <Link className="nav-links" to={`/topics/${topic.slug}/articles`}>
-              {topic.title}{' '}
+              {topic.title}
             </Link>
           </div>
         );
