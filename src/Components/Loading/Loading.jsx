e3f66603eb4d29d4b45e588d258e3e36d7b31acc
Andrew Mather <Andy-mather@live.co.uk>
2018-12-02 14:34:49 +0000

    change loader style

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -4,7 +4,7 @@ import Loader from 'react-loader-spinner';
 const Loading = props => {
   return (
     <div className="loading">
-      <Loader type="Triangle" color="#00BFFF" height="200" width="200" />;
+      <Loader type="Circles" color="#00BFFF" height="200" width="200" />
     </div>
   );
 };
